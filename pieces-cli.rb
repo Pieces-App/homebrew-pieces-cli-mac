c192f2c4d192b1ee26c97dbe53cc1e81ff9ca9dc
Connor O'Leary <101361738+connor-oleary-pieces@users.noreply.github.com>
2022-03-28 17:13:15 -0400

    Update pieces-cli.rb

--- pieces-cli.rb
+++ pieces-cli.rb
@@ -1,7 +1,7 @@
 class PiecesCli < Formula
   desc "A command line application for Pieces"
   homepage "https://code.pieces.app/"
-  url "https://storage.googleapis.com/pieces-cli-production-releases/pieces-cli-0.0.2.tar.gz"
+  url "https://storage.googleapis.com/pieces-cli-production-releases/pieces-cli-0.0.3.tar.gz"
   sha256 "73da4047759f79c12f475719bdb39a9d15a83b9df49afa05c6d9ec8a16928873"
   license "MIT"
 

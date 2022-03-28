0915add4d8850bcfe93e8c6e15468ad4ac7d76e9
Connor O'Leary <connor@Connors-MacBook-Pro-2.local>
2022-03-28 17:00:33 -0400

    build of pieces-mac;

--- pieces-cli.rb
+++ pieces-cli.rb
@@ -2,7 +2,7 @@ class PiecesCli < Formula
   desc "A command line application for Pieces"
   homepage "https://code.pieces.app/"
   url "https://storage.googleapis.com/pieces-cli-production-releases/pieces-cli-0.0.2.tar.gz"
-  sha256 "85ad00287431d08f37ba98f04ebac93777ae9f6bb1586fe197747bdb6ee66d40"
+  sha256 "73da4047759f79c12f475719bdb39a9d15a83b9df49afa05c6d9ec8a16928873"
   license "MIT"
 
   def install
